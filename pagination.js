66b82234fcfb9b50023d1c99f65a10ecf4840385
shutter-cp <37921589+shutter-cp@users.noreply.github.com>
2018-12-19 21:12:52 +0800

    Create pagination.js

---
+++ pagination.js
@@ -0,0 +1,81 @@
+/**
+ * htmlsl  分页需要重复的前段代码
+ * htmlsr  分页需要重复的后段代码
+ * objs    分页元素的id的"#??"或者class的".??"
+ * steps   步长，每次分页显示几个
+ * numbers 一共有多少页
+ * 
+ * 
+ * inits 初始化
+ * fyLeft 往前翻
+ * fyRight 往后翻
+ */
+pageInaTion = function(htmlsl,htmlsr,objs,steps,numbers){
+	var obj = {};
+	obj.htmlsl = htmlsl;
+	obj.htmlsr = htmlsr;
+	obj.steps = steps;
+	obj.numbers = numbers;
+	obj.objs = objs;
+	
+	var i = 1;
+	
+	//初始化
+	obj.inits = function(){
+		var fyHtml = '';
+		if(numbers>=steps){
+			for(;i<=steps;i++){
+				fyHtml += htmlsl+ i+ htmlsr;
+			}
+		}else{
+			for(;i<=numbers;i++){
+				fyHtml += htmlsl+ i+ htmlsr;
+			}
+		}
+		$(objs).html("");
+		$(objs).html(fyHtml);
+	}
+	
+	//往前翻
+	obj.fyLeft = function(){
+		fyHtml = '';
+		if((i-(steps*2))>0){
+			var temp = i%steps;
+			if(temp!=1){
+				i = i - temp -steps +1;
+				for(var y=i;i<y+steps;i++){
+					fyHtml += htmlsl+ i+ htmlsr;
+				}
+				$(objs).html("");
+				$(objs).html(fyHtml);
+			}else{
+				i = i-10;
+				for(var y=i;i<y+steps;i++){
+					fyHtml += htmlsl+ i+ htmlsr;
+				}
+				$(objs).html("");
+				$(objs).html(fyHtml);
+			}
+		}
+	}
+	
+	//往后翻
+	obj.fyRight = function(){
+		fyHtml = '';
+		if(i+steps<=numbers){
+			for(var y=i;i<y+steps;i++){
+				fyHtml += htmlsl+ i+ htmlsr;
+			}
+			$(objs).html("");
+			$(objs).html(fyHtml);
+		}else if(i+steps>numbers&&i<numbers){
+			for(;i<=numbers;i++){
+				fyHtml += htmlsl+ i+ htmlsr;
+			}
+			i--;
+			$(objs).html("");
+			$(objs).html(fyHtml);
+		}
+	}
+	return obj;
+}
